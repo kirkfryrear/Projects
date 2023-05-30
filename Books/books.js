0add5ee55517fc504f89e24c68344d30ad055374
Kirk Fryrear <kirk.fryrear@gmail.com>
2023-05-30 00:18:22 -0700

    Initial set up, HTML/JS, gitconfig

---
+++ Books/books.js
@@ -0,0 +1,9 @@
+async function logJSONData() {
+    const response = await fetch('https://openlibrary.org/api/books?bibkeys=ISBN:0451526538&format=json');
+    const jsonData = await response.json();
+    console.log(jsonData);
+
+    // returns string version in "results" div
+        // const jsonDataString = JSON.stringify(jsonData);
+        //document.getElementById("results").innerHTML = jsonDataString;
+}
