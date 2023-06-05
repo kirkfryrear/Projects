a38047779ed9f811d134cbf3c849de8c4a6727b2
Kirk Fryrear <kirk.fryrear@gmail.com>
2023-06-05 13:17:49 -0700

    New Cuk YT JS, no center align, re-added genre

--- Books/books.js
+++ Books/books.js
@@ -1,8 +1,12 @@
-async function logJSONData() {
-    const response = await fetch("http://openlibrary.org/api/books?bibkeys=ISBN:0451526538&jscmd=data");
-    const data = await response.json;
-    const x = JSON.parse(data);
-
-    console.log(x);
-    document.getElementById("results").innerHTML = x;
+function getBook() {
+    document.getElementById("output").innerHTML = "";
+    fetch("https://openlibrary.org/search.json?q=julius")
+    .then(a => a.json())
+    .then(response => {
+        for(var i=0;i<10;i++) {
+            document.getElementById("output").innerHTML+=
+                "<h2>"+response.docs[i].title+"</h2>"
+                +"<em>by "+response.docs[i].author_name[0]+"</em>"
+        }
+    });
 }
