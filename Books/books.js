439fe59b3f210e921371479583295b1a29b64914
Kirk Fryrear <kirk.fryrear@gmail.com>
2023-06-05 23:26:17 -0700

    centered display, working author omit

--- Books/books.js
+++ Books/books.js
@@ -1,12 +1,14 @@
 function getBook() {
+    const genreValue = document.getElementById("bookGenre").value;
     document.getElementById("output").innerHTML = "";
-    fetch("https://openlibrary.org/search.json?q=julius")
+    fetch("https://openlibrary.org/search.json?subject="+genreValue)
     .then(a => a.json())
     .then(response => {
-        for(var i=0;i<10;i++) {
-            document.getElementById("output").innerHTML+=
-                "<h2>"+response.docs[i].title+"</h2>"
-                +"<em>by "+response.docs[i].author_name[0]+"</em>"
+        for(var i=0;i<1;i++) {
+            const bookImage = "<img src='http://covers.openlibrary.org/b/isbn/"+response.docs[i].isbn[0]+"-M.jpg' />";
+            const bookAuthor = response.docs[i].author_name ? " by <span class='author'>"+response.docs[i].author_name[0]+"</span>" : "";
+            const bookInfo = "<h2>"+response.docs[i].title+" ("+response.docs[i].first_publish_year+")"+bookAuthor+"</h2>";
+            const bookOutput = document.getElementById("output").innerHTML+=bookImage+bookInfo;
         }
     });
 }
