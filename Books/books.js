76769cd8a838b7dd4bbe3097442dd97ea16285b2
Kirk Fryrear <kirk.fryrear@gmail.com>
2023-06-14 23:15:28 -0700

    can't pass 1st function result to 2nd function, thens don't work

--- Books/books.js
+++ Books/books.js
@@ -1,18 +1,23 @@
-function getBook() {
-    document.getElementById("output").innerHTML = "";
-    const subjectUrl = "https://openlibrary.org/subjects/"
-    const genreValue = document.getElementById("bookGenre").value;
-    fetch (subjectUrl+genreValue+".json")
-    .then(a => a.json())
-    .then(response => {
-        const workCount = response.work_count - 1;
-        const randomOffset = Math.floor(Math.random() * (workCount - 1 + 1)) + 1;
-        return fetch(subjectUrl+genreValue+".json?limit=1&offset="+randomOffset)
-    })
-    .then(response => response.json())
-    .then(response => {
-      const bookUrl = "https://openlibrary.org"+response.works[0].key; //fix URL later
-      const bookInfo = "<h2 class='book-title'><a href='"+bookUrl+"' target='_blank'>"+response.works[0].title+"</a> by "+response.works[0].authors[0].name+" ("+response.works[0].first_publish_year+")</h2>";
-      const bookOutput = document.getElementById("output").innerHTML+=bookInfo;
+const subjectWorkCount = 0
+
+async function getCount() {
+  const genreValue = document.getElementById("bookGenre").value
+  const subjectResponse = await fetch ("https://openlibrary.org/subjects/"+genreValue+".json")
+  const subjectData = await subjectResponse.json()
+  const subjectWorkCount = await subjectData.work_count   // produces needed number
+  return subjectWorkCount
+}
+
+async function getBook() {
+  const subjectWorkCount = getCount()
+  console.log(subjectWorkCount)
+  const genreValue = document.getElementById("bookGenre").value
+  const randomOffset = (Math.floor(Math.random() * (workCount - 1 + 1)) + 1) - 1
+  fetch("https://openlibrary.org/subjects/"+genreValue+".json?limit=1&offset="+randomOffset)
+  .then(response => response.json())
+  .then(response => {
+    const bookUrl = "https://openlibrary.org"+response.works[0].key
+    const bookInfo = "<h2 class='book-title'><a href='"+bookUrl+"' target='_blank'>"+response.works[0].title+"</a> by "+response.works[0].authors[0].name+" ("+response.works[0].first_publish_year+")</h2>"
+    const bookOutput = document.getElementById("output").innerHTML+=bookInfo
   })
 }
