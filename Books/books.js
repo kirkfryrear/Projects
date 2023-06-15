7efd10bd988bf02a366d424628a5b882f5dcbeae
Kirk Fryrear <kirk.fryrear@gmail.com>
2023-06-15 11:15:22 -0700

    added ternary when author is blank

--- Books/books.js
+++ Books/books.js
@@ -1,21 +1,23 @@
 function getBook() {
-  document.getElementById("output").innerHTML = "";
+  const import_button = document.getElementById("bookBtn")
+  import_button.disabled = true
+  document.getElementById("output").innerHTML = ""
   const subjectUrl = "https://openlibrary.org/subjects/"
-  const genreValue = document.getElementById("bookGenre").value;
-  const import_button = document.getElementById("bookBtn");
+  const genreValue = document.getElementById("bookGenre").value
   fetch (subjectUrl+genreValue+".json")
   .then(a => a.json())
   .then(response => {
-      import_button.disabled = true
-      const workCount = response.work_count - 1;
-      const randomOffset = Math.floor(Math.random() * (workCount - 1 + 1)) + 1;
+      const workCount = response.work_count - 1
+      const randomOffset = Math.floor(Math.random() * (workCount - 1 + 1)) + 1
       return fetch(subjectUrl+genreValue+".json?limit=1&offset="+randomOffset)
   })
   .then(response => response.json())
   .then(response => {
-    const bookUrl = "https://openlibrary.org"+response.works[0].key; //fix URL later
-    const bookInfo = "<h2 class='book-title'><a href='"+bookUrl+"' target='_blank'>"+response.works[0].title+"</a> by "+response.works[0].authors[0].name+" ("+response.works[0].first_publish_year+")</h2>";
-    const bookOutput = document.getElementById("output").innerHTML+=bookInfo;
+    const bookUrl = "https://openlibrary.org"+response.works[0].key
+    const authorInfo = response.works[0].authors[0] ? " by <a target='_blank' href='https://openlibrary.org"+response.works[0].authors[0].key+" target='_blank'>"+response.works[0].authors[0].name+"</a>" : ""
+    const bookInfo = "<h2><a class='book-title' href='"+bookUrl+"' target='_blank'>"+response.works[0].title+"</a><br />"+authorInfo+" ("+response.works[0].first_publish_year+")</h2>"
+    const bookOutput = document.getElementById("output").innerHTML+=bookInfo
     import_button.disabled = false
+    console.log(response.works[0])
   })
 }
